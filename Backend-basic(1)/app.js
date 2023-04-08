dcb93e02c01edde3c5e11796bcba0dec3c9951aa
omkundu <omkundu156@gmail.com>
2023-04-08 16:19:06 +0530

    fourth-commit

--- Backend-basic(1)/app.js
+++ Backend-basic(1)/app.js
@@ -1,8 +1,11 @@
 const {sum,mult,sub,divide}=require('./calc')
-console.log(sum(2,3))   //5
-console.log(mult(2,3))//6
+// console.log(sum(2,3))   //5
+// console.log(mult(2,3))//6
 
-console.log(sub(2,3))//-1
+// console.log(sub(2,3))//-1
 
-console.log(divide(2,3))
+// console.log(divide(2,3))
+
+const os=require("os");
+console.log(os.freemem())
 
