f5a9e7a2b99d664a19e777770752b3735b13b07e
omkundu <omkundu156@gmail.com>
2023-04-08 14:50:08 +0530

    third-commit

--- Backend-basic(1)/calc.js
+++ Backend-basic(1)/calc.js
@@ -2,4 +2,15 @@ const sum=(a,b)=>{
     return a+b;
 }
 
-module.exports=(sum)
+const mult=(a,b)=>{
+    return a*b;
+}
+
+const sub=(a,b)=>{
+    return a-b;
+}
+
+const divide=(a,b)=>{
+    return a/b;
+}
+module.exports={sum,mult,sub,divide}
